e0dd6d5894e09d9ca4254ca1134dd64550d4bc3c
772796015 <772796015@qq.com>
2020-06-15 16:17:07 +0800

    增加post请求，接受为实体类的例子。见文档章节2.3

--- src/main/java/com/example/demo/example/PostController.java
+++ src/main/java/com/example/demo/example/PostController.java
@@ -1,22 +1,23 @@
 package com.example.demo.example;
 
+import com.example.demo.vo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 演示postman post请求示例
+ * 演示postman post请求示例 详见聚合项目2.3章节
  * Created by DJ010199 on 2020/5/21.
  */
 
-// post请求示例  postman上访问 url：localhost:8080/test1，params栏填 key：username，value：111.key：password，value：222    输入以上信息后访问即可
 @RestController
 public class PostController {
     private Logger logger = LoggerFactory.getLogger(PostController.class);
 
-
+// post请求示例  postman上访问 url：localhost:8080/test1，params栏填 key：username，value：111.key：password，value：222    输入以上信息后访问即可
 @PostMapping(value = "/test1")
 public String test1(@RequestParam(name = "username") String username,
                     @RequestParam(name = "password") String password) {
@@ -24,4 +25,25 @@ public String test1(@RequestParam(name = "username") String username,
     System.out.println(username+""+password);
     return "成功";
 }
+
+    // post请求示例  类似于表单提交 postman上访问 url：localhost:8080/testVO，
+    // params栏填 key：id，value：1.key：name，value：张三 .key：age，value：1.key：address，value：1 . key：phone，value：18510808831
+    //以上就是实体类User属性，输入以上信息后访问即可
+    @PostMapping(value = "/testVO")
+    public String testVO(User user) {
+        logger.info(user.getName()+"---"+user.getPhone());
+        System.out.println(user.getName()+"---"+user.getPhone());
+        return "成功";
+    }
+
+    //  post请求示例  举例：ajaxjson串请求 postman上访问 url：localhost:8080/testJson，
+    //body页签选择raw模式，数据类型选择json（application/json），  传参：{	"id":"1",	"name":"张三",	"age":"10",	"address":"20",	"phone":"18510808831"}
+
+    @PostMapping(value = "/testJson")
+    public String testJson(@RequestBody User user) {
+        logger.info(user.getName()+"---"+user.getPhone());
+        System.out.println(user.getName()+"---"+user.getPhone());
+        return "成功";
+    }
+
 }
