72558fac07d610019cc97dd560d1a7a5ebad027b
DJ010199 <772796015@qq.com>
2021-04-22 13:53:51 +0800

    22 AOP实现自定义注解--增加参数解释

--- src/main/java/com/example/demo/annotation/Log.java
+++ src/main/java/com/example/demo/annotation/Log.java
@@ -8,5 +8,5 @@ import java.lang.annotation.RetentionPolicy;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Log {
-    String value() default "";
+    String value() default "";//参数列表，此处会在LogAspect方法中log.value()调用获取 注解@Log(value="-------------------自定义注解实现了!!!------------")的值
 }
--- src/main/java/com/example/demo/example/LogController.java
+++ src/main/java/com/example/demo/example/LogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LogController {
     //具体实现可参考文章：https://blog.csdn.net/qq_37976540/article/details/105600894
     // 自定义注解示例 postman上访问 url：http://localhost:8080/logController/123?name=zhang  输入以上信息后访问即可
-    @Log("-------------------自定义注解实现了!!!------------")
+    @Log(value="-------------------自定义注解实现了!!!------------")//参数value
     @GetMapping("/logController/{id}")
     public String demo(@PathVariable(name = "id") String id, @RequestParam(name = "name") String name) {
         System.out.println("id="+id);
