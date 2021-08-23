b6bb9e3f81bb76045baf83af5da90a30c897fdd1
t-mac <772796015@qq.com>
2021-08-23 16:29:18 +0800

    规范输出日志

--- src/main/java/com/example/demo/example/GetController.java
+++ src/main/java/com/example/demo/example/GetController.java
@@ -1,5 +1,7 @@
 package com.example.demo.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetController {
+    private final static Logger logger = LoggerFactory.getLogger(GetController.class);
     // get请求示例  postman上访问 url：http://localhost:8080/getController/123?name=zhang  输入以上信息后访问即可
     @GetMapping("/getController/{id}")
     public String demo(@PathVariable(name = "id") String id, @RequestParam(name = "name") String name) {
-        System.out.println("id="+id);
-        System.out.println("name="+name);
+        logger.info("id="+id);
+        logger.info("name="+name);
         return id+"------"+name;
     }
 
