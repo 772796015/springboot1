96810e674cb53e9c94392f576d83161b9988fd69
t-mac <772796015@qq.com>
2021-09-11 18:36:13 +0800

    9.redis存取对象

--- src/main/java/com/example/demo/example/RedisController.java
+++ src/main/java/com/example/demo/example/RedisController.java
@@ -1,5 +1,6 @@
 package com.example.demo.example;
 
+import com.example.demo.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RedisController {
     @Autowired
-    private RedisTemplate<String,String>redisTemplate;
+    private RedisTemplate<Object,Object> redisTemplate;
 
     @RequestMapping("/redis")
     public String getKeyValue(){
+        //操作对象
+        User user =new User();
+        user.setAge(20);
+        user.setId(1);
+        redisTemplate.opsForValue().set("user",user);
+        User value1=(User)redisTemplate.opsForValue().get("user");
+        System.out.println(value1);
+
+
+        //操作字符串
         redisTemplate.opsForValue().set("myKey","myValue");
         String value=(String)redisTemplate.opsForValue().get("myKey");
         System.out.println(value);
